48e632b7d87d1604ccad0ac4f5429aba57e5c65c
fellah23-science <rahnamohemmed@gmail.com>
2026-04-30 16:21:30 +0530

    update exohabit.py

--- background.py
+++ background.py
@@ -69,9 +69,9 @@ if mode == "🌟 Basic":
 
     tab1, tab2, tab3, tab4 = st.tabs(["🌍 Create Planet", "🧠 Quiz", "🏆 Progress", "🥇 Leaderboard"])
 
-    # -------- CREATE PLANET --------
+# -------- CREATE PLANET --------
   
-  with tab1:
+with tab1:
     st.header("🌍 Create Your Planet (Fixed System)")
 
     # Inputs
